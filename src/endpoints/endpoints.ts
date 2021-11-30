8314e2b944b8a81ce37fdd23e4e5c9485712c6f8
Ubuntu <ubuntu@ip-172-31-23-62.us-east-2.compute.internal>
2021-11-30 13:26:28 +0000

    change ip address of graphdb

--- src/endpoints/endpoints.ts
+++ src/endpoints/endpoints.ts
@@ -1,10 +1,10 @@
 import active from "./active"
 import commit from "./commit"
 import rollback from "./rollback"
-import isPresent from ".//isPresent"
+import isPresent from "./isPresent"
 import { Endpoint } from "../server"
 import writeToLearnerRecord from "./writeToLearnerRecord"
 
-const endpoints: Array <Endpoint> = [active, commit, rollback, isPresent, writeToLearnerRecord]
+const endpoints: Array<Endpoint> = [active, commit, rollback, isPresent, writeToLearnerRecord]
 
 export default endpoints
