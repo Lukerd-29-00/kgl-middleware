dd6b46c1e5630e46bece55c44c475f79113638c5
lucasd <Driscoll_Lucas@bah.com>
2022-01-19 18:28:27 -0500

    Added some code documentation

--- src/endpoints/active.ts
+++ src/endpoints/active.ts
@@ -5,6 +5,15 @@ import {ParamsDictionary, Query} from "express-serve-static-core"
 
 const route = "/active"
 
+/**
+ * Processes an incoming request to the /active resource by determining if the Graphdb server pointed to in config.ts is reachable. Calls next() if it is, and next(e) if it encounters an error e reaching the server.
+ * @param request The request sent to the /active resource
+ * @param response The response to request
+ * @param next A callback that calls the next function in the middleware stack
+ * @param ip The url of the graphdb server
+ * @param defaultRepo The desired repository
+ * @async
+ */
 async function processActive(request: Request<ParamsDictionary,string,Record<string,string | number | boolean | undefined>,Query>, response: Response<string>, next: (e?: Error) => void, ip: string, defaultRepo: string): Promise<void>{
     fetch(`${ip}/repositories/${defaultRepo}/size`).then(probe => {
         if(!probe.ok){
